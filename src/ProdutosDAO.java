0f1cd5b5deefb0db9ed130a4fd4b2a4bd21e9f11
Thayná Barbosa dos Santos <thaynasants@gmail.com>
2023-07-09 21:54:40 -0300

    Commit #3: filtro para listar apenas itens vendidos.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,7 +7,6 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -15,34 +14,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-                
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
         conn = new conectaDAO().connectDB();
-        
-        try{
+
+        try {
             prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");
             prep.setString(1, produto.getNome());
             prep.setInt(2, produto.getValor());
             prep.setString(3, produto.getStatus());
             prep.execute();
             JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Erro ao cadastrar: " + ex.getMessage());
             JOptionPane.showMessageDialog(null, "Não foi possivel realizar o cadastro!");
         }
-        
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
         conn = new conectaDAO().connectDB();
         String sql = "SELECT id, nome, valor, status FROM produtos";
 
@@ -50,7 +47,7 @@ public class ProdutosDAO {
             prep = conn.prepareStatement(sql);
             resultset = prep.executeQuery();
 
-           listagem = new ArrayList<>();
+            listagem = new ArrayList<>();
 
             while (resultset.next()) {
                 ProdutosDTO prod = new ProdutosDTO();
@@ -59,33 +56,52 @@ public class ProdutosDAO {
                 prod.setNome(resultset.getString("nome"));
                 prod.setValor(resultset.getInt("valor"));
                 prod.setStatus(resultset.getString("status"));
-          
+
                 listagem.add(prod);
             }
-        return listagem;
-    }
-    catch (Exception e) {
+            return listagem;
+        } catch (Exception e) {
             return null;
         }
-    
-    
+
     }
-    
-    
-    public void venderProduto(String id){
+
+    public void venderProduto(String id) {
         conn = new conectaDAO().connectDB();
         String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
-        
-        try{
+
+        try {
             prep = conn.prepareStatement(sql);
             prep.setString(1, id);
             prep.execute();
-        
+            JOptionPane.showMessageDialog(null, "Produto Vendido!");
         } catch (Exception e) {
             System.out.println("Erro ao editar status do produto: " + e.getMessage());
         }
     }
-    
-    
-}
 
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido';";
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            listagem = new ArrayList<>();
+
+            while (resultset.next()) {
+                ProdutosDTO prod = new ProdutosDTO();
+
+                prod.setId(resultset.getInt("id"));
+                prod.setNome(resultset.getString("nome"));
+                prod.setValor(resultset.getInt("valor"));
+                prod.setStatus(resultset.getString("status"));
+
+                listagem.add(prod);
+            }
+            return listagem;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
--- src/vendasVIEW.java
+++ src/vendasVIEW.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -15,6 +19,7 @@ public class vendasVIEW extends javax.swing.JFrame {
      */
     public vendasVIEW() {
         initComponents();
+        atualizarTabela();
     }
 
     /**
@@ -148,4 +153,27 @@ public class vendasVIEW extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable listaVendidos;
     // End of variables declaration//GEN-END:variables
+
+    private void atualizarTabela(){
+        try {
+            ProdutosDAO produtosdao = new ProdutosDAO();
+            
+            DefaultTableModel model = (DefaultTableModel) listaVendidos.getModel();
+            model.setNumRows(0);
+            
+            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutosVendidos();
+            
+            for(int i = 0; i < listagem.size(); i++){
+                model.addRow(new Object[]{
+                    listagem.get(i).getId(),
+                    listagem.get(i).getNome(),
+                    listagem.get(i).getValor(),
+                    listagem.get(i).getStatus()
+                });
+            }
+        } catch (Exception e) {
+        }
+    
+    }
+
 }
